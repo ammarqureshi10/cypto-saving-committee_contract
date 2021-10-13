67c9c448e6340c0441b494cb7a6fb40a2daee9eb
ammarqureshi10 <58142882+ammarqureshi10@users.noreply.github.com>
2021-10-13 12:53:01 -0700

    Update CommitteeContract.sol

--- contracts/CommitteeContract.sol
+++ contracts/CommitteeContract.sol
@@ -25,7 +25,7 @@ contract CommitteeSystem is CommitteeSystemInterface{
     
     event Withdrawal(address to,uint256 amount);
     event NewCommitteeCreated(uint256 committeeNo, uint256 minimumDeposit, uint256 MaxParticipants);
-    event Participated(address by);
+    event Participated(address by, uint256 _committeeNo);
     
     function createNewCommittee(uint256 _minimumDeposit, uint256 _MaxParticipants) external override returns(bool){
         require(_minimumDeposit > 0, "minimumDeposit should be greater than 0");
@@ -59,7 +59,7 @@ contract CommitteeSystem is CommitteeSystemInterface{
         
         committeeCreator[_committeeNo].currentTotal += msg.value;
         
-        emit Participated(msg.sender);
+        emit Participated(msg.sender, _committeeNo);
         return true;
     }
     
